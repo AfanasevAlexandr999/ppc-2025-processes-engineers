ee233c13a5a021597bcf06a666049c0fa67d6a89
AfanasevAlexandr999 <prostosaz2.0@gmail.com>
2025-11-13 17:54:00 +0300

    Commit lec 2

--- tasks/afanasyev_a_elem_vec_avg/tests/performance/main.cpp
+++ tasks/afanasyev_a_elem_vec_avg/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace afanasyev_a_elem_vec_avg {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class AfanasyevAElemVecAvgPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(AfanasyevAElemVecAvgPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = AfanasyevAElemVecAvgPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, AfanasyevAElemVecAvgPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace afanasyev_a_elem_vec_avg
